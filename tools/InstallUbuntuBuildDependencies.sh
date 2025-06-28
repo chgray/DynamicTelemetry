84e1f77d863817afae463cc89276f8c6955a3d24
Chris Gray <chgray@gmail.com>
2025-06-28 09:43:31 -0700

    experimenting with cloud build

--- tools/InstallUbuntuBuildDependencies.sh
+++ tools/InstallUbuntuBuildDependencies.sh
@@ -13,14 +13,28 @@ export DEBIAN_FRONTEND=noninteractive
 apt_packages="git podman dotnet8"
 echo "Detecting / Installing necessary Ubuntu tools"
 if ! command -v git &> /dev/null; then
+    echo "Installing Git"
     apt update
     apt install -y ${apt_packages}
 fi
 if ! command -v podman &> /dev/null; then
+    echo "Installing podman"
     apt update
     apt install -y ${apt_packages}
 fi
 if ! command -v dotnet &> /dev/null; then
+    echo "Installing dotnet"
     apt update
     apt install -y ${apt_packages}
 fi
+
+echo "Versions of tools:"
+echo "---------------------------"
+echo "Git Version:"
+git --version
+
+echo "Podman version:"
+podman --version
+
+echo "Dotnet version:"
+dotnet --version
